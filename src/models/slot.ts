fe63128da663d29daa841622371cfca9171687cb
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-01-12 00:46:45 +0200

    Add winning lines validation and update Slot class constructor

--- src/models/slot.ts
+++ src/models/slot.ts
@@ -1,4 +1,5 @@
 import {
+  winingLinesValidation,
   reelsCountValidation,
   rowsCountValidation,
   symbolsValuesValidation,
@@ -8,11 +9,18 @@ export default class Slot {
   private _reelsCount: number | null = null;
   private _rowsCount: number | null = null;
   private _symbolsValues: object | null = null;
+  private _winingLines: number[][] | null = null;
 
-  constructor(reelsCount: number, rowsCount: number, symbolsAndPrizes: object) {
+  constructor(
+    reelsCount: number,
+    rowsCount: number,
+    symbolsAndPrizes: object,
+    winingLines: number[][]
+  ) {
     this.setReelsCount(reelsCount);
     this.setRowsCount(rowsCount);
     this.setSymbolsValues(symbolsAndPrizes);
+    this.setWiningLines(winingLines);
   }
 
   public get reelsCount() {
@@ -25,6 +33,10 @@ export default class Slot {
 
   public get symbolsValues() {
     return this._symbolsValues;
+  }
+
+  public get winingLines() {
+    return this._winingLines;
   }
 
   private setReelsCount(reels: number) {
@@ -42,13 +54,23 @@ export default class Slot {
     this._symbolsValues = symbols;
   }
 
+  private setWiningLines(lines: number[][]) {
+    winingLinesValidation(lines);
+    this._winingLines = lines;
+  }
+
   public Spin() {
-    return `Rows: ${this.rowsCount}\nReels: ${this.reelsCount}\nSymbols: ${
-      this.symbolsValues
-        ? `${Object.entries(this.symbolsValues).map((item) => {
-            return `\nSymbol ${item[0]}: ${item[1]}`;
-          })}`
-        : 'No symbols'
-    }\n game result`;
+    return `Rows: ${this.rowsCount}
+    \nReels: ${this.reelsCount}
+    \nSymbols: ${
+      this.symbolsValues &&
+      `${Object.entries(this.symbolsValues).map((item) => {
+        return `\nSymbol ${item[0]}: ${item[1]}`;
+      })}`
+    }
+    \nWining Lines: ${
+      this.winingLines && `${this.winingLines?.map((line) => `\n${line}`)}`
+    }
+    \ngame result`;
   }
 }
