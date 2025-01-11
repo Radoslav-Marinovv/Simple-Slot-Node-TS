2e16e1ada3bf4aecddc998db49830896801b52e4
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-01-11 21:00:36 +0200

    Implement slot machine functionality by importing configuration and initializing Slot instance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import Slot from './models/slot.js';
+import * as slotMachineConfig from './configurations/configuration.js';
+
+const { reelsCount, rowsCount, symbols, lines, reels } =
+  slotMachineConfig.default;
+
+const slot = new Slot(reelsCount, rowsCount);
+
+console.log(slot.Spin());
