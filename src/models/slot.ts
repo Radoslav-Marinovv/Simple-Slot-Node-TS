99fab6deae37b0b03f682abf62dd07cdcc4deced
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-01-12 00:24:14 +0200

    Add symbols values validation and update Slot class constructor

--- src/models/slot.ts
+++ src/models/slot.ts
@@ -1,15 +1,18 @@
 import {
   reelsCountValidation,
   rowsCountValidation,
+  symbolsValuesValidation,
 } from '../validation/slot/slot-validation.js';
 
 export default class Slot {
   private _reelsCount: number | null = null;
   private _rowsCount: number | null = null;
+  private _symbolsValues: object | null = null;
 
-  constructor(reelsCount: number, rowsCount: number) {
+  constructor(reelsCount: number, rowsCount: number, symbolsAndPrizes: object) {
     this.setReelsCount(reelsCount);
     this.setRowsCount(rowsCount);
+    this.setSymbolsValues(symbolsAndPrizes);
   }
 
   public get reelsCount() {
@@ -18,6 +21,10 @@ export default class Slot {
 
   public get rowsCount() {
     return this._rowsCount;
+  }
+
+  public get symbolsValues() {
+    return this._symbolsValues;
   }
 
   private setReelsCount(reels: number) {
@@ -30,7 +37,18 @@ export default class Slot {
     this._rowsCount = rows;
   }
 
+  private setSymbolsValues(symbols: object) {
+    symbolsValuesValidation(symbols);
+    this._symbolsValues = symbols;
+  }
+
   public Spin() {
-    return `Rows: ${this.rowsCount}\nReels: ${this.reelsCount}\n game result`;
+    return `Rows: ${this.rowsCount}\nReels: ${this.reelsCount}\nSymbols: ${
+      this.symbolsValues
+        ? `${Object.entries(this.symbolsValues).map((item) => {
+            return `\nSymbol ${item[0]}: ${item[1]}`;
+          })}`
+        : 'No symbols'
+    }\n game result`;
   }
 }
