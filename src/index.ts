a082644ee01be359c9bec9359354c16d69dc085d
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-01-12 20:55:14 +0200

    Add balance check in spin loop to prevent betting with insufficient funds

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ const slot = new Slot(reelsCount, rowsCount, symbols, lines, reels);
 let playerBalance = 1000;
 let playerBet = 10;
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 102; i++) {
+  if (playerBalance < playerBet) {
+    console.log('\nNot enough balance to play');
+    break;
+  }
   playerBalance += slot.Spin(playerBalance, playerBet);
 }
